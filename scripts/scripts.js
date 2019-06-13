7db70e8b02b3e71b1d398c931021963fdbce2c61
Andreea <41521522+AndreeaCcss@users.noreply.github.com>
2019-06-13 22:24:09 +0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,36 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Andreea Cucos';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/rhea-in-snow.jpg') {
+      myImage.setAttribute ('src','images/rhea-warrior-pose.jpg');
+    } else {
+      myImage.setAttribute ('src','images/rhea-in-snow.jpg');
+    }
+};
+
+
+// document.querySelector('html').onclick = function() {
+//     alert('Ouch! Stop poking me!');
+// }
+
+// var myButton = document.querySelector('button');
+// var myHeading = document.querySelector('h1');
+// function setUserName() {
+//     var myName = prompt('Please enter your name.');
+//     localStorage.setItem('name', myName);
+//     myHeading.textContent = 'Mozilla is cool, ' + myName;
+// };
+// // initialization on load 
+// if(!localStorage.getItem('name')) {
+//     setUserName();
+// } else {
+//     var storedName = localStorage.getItem('name');
+//     myHeading.textContent = 'Mozilla is cool, ' + storedName;
+// }
+// myButton.onclick = function() {
+//     setUserName();
+// }
